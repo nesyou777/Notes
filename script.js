2366c35261f881d066de833197eb855f09be6c8d
nesyou777 <moustamid7@outlook.com>
2026-02-06 21:20:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ const WALL_POSITIONS = [
 const COLORS = ["sticky-yellow", "sticky-pink", "sticky-blue", "sticky-white", "sticky-green"];
 
 /* ✍️ typing animation (SLOWER by default) */
-function typeText(fullText, speed = 70) { // higher = slower
+function typeText(fullText, speed = 80) { // higher = slower
   if (typingTimer) clearInterval(typingTimer);
 
   noteTextEl.classList.add("typing");
@@ -205,4 +205,5 @@ async function init() {
 }
 
 init().catch(console.error);
+
 
